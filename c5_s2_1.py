d4ede617150ecb1235ecf0179e21e7d971c9175a
Yuta Takahashi <m1205123ing@gmail.com>
2018-01-20 13:26:33 +0900

    add result at c5_s2_1

---
+++ c5_s2_1.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import sys
+import math
+
+
+# 素数チェック
+def check_prime(val, type=1):
+    try:
+        val = int(val)
+
+        if val <= 1:
+            return False
+
+        if int(type) == 1:
+            if is_prime1(val):
+                return True
+        elif int(type) == 2:
+            if is_prime2(val):
+                return True
+        return False
+    except ValueError:
+        print("ERROR: 数値を入力してください")
+
+
+# 1つずつ回してく
+def is_prime1(val):
+    print(">>version1")
+    for i in range(2, val):
+        if val % i == 0:
+            print("{}で割れました".format(i))
+            return False
+    return True
+
+
+# 高速化Ver
+def is_prime2(val):
+    print(">>version2")
+    if val == 2:
+        return True
+    if val % 2 == 0:
+        return False
+
+    sqrt = math.sqrt(val)
+    i = 3
+    while i <= sqrt:
+        if val % i == 0:
+            print("{}で割れました".format(i))
+            return False
+        i = i + 2
+    return True
+
+
+if __name__ == '__main__':
+    argvs = sys.argv
+    if len(argvs) not in [2, 3]:
+        print("数値を1つ入力してください")
+    else:
+        res = check_prime(argvs[1], argvs[2])
+        if res:
+            print("素数")
