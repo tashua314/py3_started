88e706b030f5cb8a338145b25fd6879b916d4bae
Yuta Takahashi <m1205123ing@gmail.com>
2018-01-24 05:03:51 +0000

    tweak

--- c5_s2_3.py
+++ c5_s2_3.py
@@ -30,8 +30,21 @@ def make_password(length, type=1):
 
 if __name__ == '__main__':
     argvs = sys.argv
+    # 対話式で引数入力する時
+    if len(argvs) == 1:
+        length = input('作成するパスワード長を入力してください: ')
+        make_pass_type = input('作成パスワードの種類を入力してください(1:改行文字や空白文字等含む, 2:左記文字を含まない): ')
+        password = make_password(length, make_pass_type)
+        print(password)
+        sys.exit()
+
+    # ファイル名と共に引数を渡す時
     if len(argvs) not in [2, 3]:
-        print("数値を1つ入力してください")
+        print("作成するパスワード長と作成パスワードの種類(1:改行文字や空白文字等含む, 2:左記文字を含まない)を入力してください")
         sys.exit(1)
-    password = make_password(argvs[1], argvs[2])
+
+    if len(argvs) == 2:
+        password = make_password(argvs[1])
+    else:
+        password = make_password(argvs[1], argvs[2])
     print(password)
