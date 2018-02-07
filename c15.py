9d0038ee9dfcf3fbff2df736f076c649c509a2ab
Yuta Takahashi <m1205123ing@gmail.com>
2018-02-07 20:10:24 +0900

    c15

---
+++ c15.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+
+def kuku():
+    for i in range(1, 10):
+        [i * multiplier for multiplier in range(1, 10)]
+
+
+def 
+
+if __name__ == '__main__':
+    d = CountUpDict('test.txt')
+    print(d.words)
