680721f867b690cdb89fa7ce69616282aa74e955
Yuta Takahashi <m1205123ing@gmail.com>
2018-02-10 17:45:03 +0900

    update c12

--- c12.py
+++ c12.py
@@ -4,18 +4,47 @@
 class Human:
 
     def __init__(self, age, name, gender):
-        self.age = age
-        self.name = name
-        self.gender = gender
+        self.__age = age
+        self.__name = name
+        self.__gender = gender
+        self.test = 'aiu'
+        self._test2 = 'kakiku'
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        self.__age = age
+        return
+
+    @property
+    def name(self):
+        return self.__name
+
+    # @name.setter
+    # def name(self, name):
+    #     self.__name = name
+    #     return
+
+    # @property
+    # def gender(self):
+    #     return self.__gender
+
+    # @gender.setter
+    # def gender(self, gender):
+    #     self.__gender = gender
+    #     return
 
     def __lt__(self, other):
-        return self.age > other.age
+        return self.__age > other.age
 
     def __str__(self):
-        return f"<Human | {self.age}, {self.name}, {self.gender} >"
+        return f"<Human | {self.__age}, {self.__name}, {self.__gender} >"
 
     def __repr__(self):
-        return f"<Human || {self.age}, {self.name}, {self.gender} >"
+        return f"<Human || {self.__age}, {self.__name}, {self.__gender} >"
 
 
 if __name__ == '__main__':
@@ -26,3 +55,12 @@ if __name__ == '__main__':
     ]
     print(users)
     print(sorted(users))
+    print(users[0].age)
+    print(users[0].test)
+    print(users[0]._test2)
+    print(users[0]._Human__gender)
+    print(users[0].age)
+    users[0].age = 1
+    print(users[0].age)
+    # error
+    users[0].name = 'test'
